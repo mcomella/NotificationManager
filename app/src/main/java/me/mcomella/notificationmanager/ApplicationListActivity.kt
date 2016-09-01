9242744d434e9fd5b8a99cb36647a4f12eef5f35
Michael Comella <michael.l.comella@gmail.com>
2016-08-31 18:16:45 -0700

    list all installed applications.

--- app/src/main/java/me/mcomella/notificationmanager/ApplicationListActivity.kt
+++ app/src/main/java/me/mcomella/notificationmanager/ApplicationListActivity.kt
@@ -1,5 +1,7 @@
 package me.mcomella.notificationmanager
 
+import android.content.Context
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
@@ -7,6 +9,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 
 import kotlinx.android.synthetic.main.activity_application_list.*
@@ -17,7 +20,7 @@ class ApplicationListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_application_list)
 
-        application_list.adapter = ApplicationListAdapter()
+        application_list.adapter = ApplicationListAdapter(this)
         application_list.setHasFixedSize(true)
         val layoutManager = LinearLayoutManager(this)
         layoutManager.orientation = LinearLayoutManager.VERTICAL
@@ -27,11 +30,18 @@ class ApplicationListActivity : AppCompatActivity() {
 
 private class ApplicationListViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
     val title = itemView!!.findViewById(R.id.title) as TextView
+    val icon = itemView!!.findViewById(R.id.icon) as ImageView
 }
 
-private class ApplicationListAdapter : RecyclerView.Adapter<ApplicationListViewHolder>() {
+private class ApplicationListAdapter(context: Context) : RecyclerView.Adapter<ApplicationListViewHolder>() {
+    // TODO: metadata; lazy?; when to update?; filter on system pkg http://stackoverflow.com/a/8483920?; filter enabled?
+    val installedApplications = context.packageManager.getInstalledApplications(0).map {
+        val pkgManager = context.packageManager
+        AppInfo(it.packageName, it.loadLabel(pkgManager).toString(), it.loadIcon(pkgManager))
+    }.sortedBy { it.label }
+
     override fun getItemCount(): Int {
-        return 2
+        return installedApplications.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ApplicationListViewHolder {
@@ -41,7 +51,12 @@ private class ApplicationListAdapter : RecyclerView.Adapter<ApplicationListViewH
         return ApplicationListViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ApplicationListViewHolder?, position: Int) {
-        holder!!.title.text = "An application " + position
+    override fun onBindViewHolder(holder: ApplicationListViewHolder, position: Int) {
+        val app = installedApplications[position]
+        holder.title.text = app.label
+        holder.icon.setImageDrawable(app.icon)
     }
+
+    private data class AppInfo(val pkgName: String, val label: String, val icon: Drawable)
+
 }
