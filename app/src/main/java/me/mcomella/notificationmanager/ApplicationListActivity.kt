1c5ece32413f4dd85748056a006bd7d1b5c50c11
Michael Comella <michael.l.comella@gmail.com>
2016-09-02 17:10:37 -0700

    add TabLayout and ViewPager.

--- app/src/main/java/me/mcomella/notificationmanager/ApplicationListActivity.kt
+++ app/src/main/java/me/mcomella/notificationmanager/ApplicationListActivity.kt
@@ -1,18 +1,12 @@
 package me.mcomella.notificationmanager
 
-import android.content.Context
 import android.content.Intent
-import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.Switch
-import android.widget.TextView
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 
 import kotlinx.android.synthetic.main.activity_application_list.*
 
@@ -25,76 +19,39 @@ class ApplicationListActivity : AppCompatActivity() {
     }
 
     private fun attachClickListeners() {
+        /* TODO: fix permissions again.
         notification_settings_button.setOnClickListener {
             startActivity(Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"))
         }
+        */
     }
 
     override fun onStart() {
         super.onStart()
 
-        application_list.adapter = ApplicationListAdapter(this)
-        application_list.setHasFixedSize(true)
-        val layoutManager = LinearLayoutManager(this)
-        layoutManager.orientation = LinearLayoutManager.VERTICAL
-        application_list.layoutManager = layoutManager
+        tabsLayout.tabMode = TabLayout.MODE_SCROLLABLE
+        contentPager.adapter = UserContextAdapter(supportFragmentManager)
+        tabsLayout.setupWithViewPager(contentPager)
+
         startNotificationService()
     }
 
     private fun startNotificationService() {
-        // TODO: PERMISSIONS?
         val intent = Intent(this, NotificationService::class.java)
         startService(intent) // TODO: start service on device startup too.
     }
 }
 
-private class ApplicationListViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
-    val title = itemView!!.findViewById(R.id.title) as TextView
-    val icon = itemView!!.findViewById(R.id.icon) as ImageView
-    val toggle = itemView!!.findViewById(R.id.toggle) as Switch
-}
-
-private class ApplicationListAdapter(context: Context) : RecyclerView.Adapter<ApplicationListViewHolder>() {
-
-    val diskManager = DiskManager(context)
-
-    // TODO: make temp vars not properties
-    // TODO: lazy? (especially icons)
-    // TODO: filter on system pkgs? http://stackoverflow.com/a/8483920? enabled apps?
-    private val _installedApps = context.packageManager.getInstalledApplications(0)
-    private val _appsAndStateOnDisk = diskManager.readApplicationsFromDisk()
-    val installedAppInfo = _installedApps.map {
-        val pkgManager = context.packageManager
-        AppInfo(pkgName = it.packageName,
-                label = it.loadLabel(pkgManager).toString(),
-                icon = it.loadIcon(pkgManager),
-                isChecked = _appsAndStateOnDisk.getOrElse(it.packageName, { true }))
-    }.sortedBy { it.label }
-
-    override fun getItemCount(): Int {
-        return installedAppInfo.size
+private class UserContextAdapter(fragmentManager: FragmentManager) : FragmentPagerAdapter(fragmentManager) {
+    override fun getCount(): Int {
+        return 2
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ApplicationListViewHolder {
-        val view = LayoutInflater.
-                from(parent!!.context).
-                inflate(R.layout.application_item, parent, false)
-        return ApplicationListViewHolder(view)
+    override fun getItem(position: Int): Fragment {
+        return ApplicationListFragment()
     }
 
-    override fun onBindViewHolder(holder: ApplicationListViewHolder, position: Int) {
-        val app = installedAppInfo[position]
-        holder.title.text = app.label
-        holder.icon.setImageDrawable(app.icon)
-
-        holder.toggle.isChecked = app.isChecked
-        holder.toggle.setOnCheckedChangeListener { buttonView, isChecked ->
-            // Thread-safe: only updated from UI thread.
-            app.isChecked = isChecked
-            diskManager.saveApplicationsToDisk(installedAppInfo.associateBy({it.pkgName}, {it.isChecked})) // TODO: converting each time is slow.
-        }
+    override fun getPageTitle(position: Int): CharSequence {
+        return "Page $position"
     }
-
-    // TODO: This name collides with ApplicationInfo.
-    private data class AppInfo(val pkgName: String, val label: String, val icon: Drawable, var isChecked: Boolean)
 }
---
+++ app/src/main/java/me/mcomella/notificationmanager/ApplicationListFragment.kt
@@ -0,0 +1,86 @@
+package me.mcomella.notificationmanager
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Switch
+import android.widget.TextView
+
+class ApplicationListFragment() : Fragment() {
+    var applicationList: RecyclerView? = null
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val rootView = inflater!!.inflate(R.layout.fragment_application_list, container, false)
+        initApplicationList(rootView)
+        return rootView
+    }
+
+    private fun initApplicationList(rootView: View) {
+        val tmpApplicationList = rootView.findViewById(R.id.applicationList) as RecyclerView
+        tmpApplicationList.adapter = ApplicationListAdapter(context)
+        tmpApplicationList.setHasFixedSize(true)
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        tmpApplicationList.layoutManager = layoutManager
+
+        applicationList = tmpApplicationList // TODO: useful?
+    }
+}
+
+private class ApplicationListAdapter(context: Context) : RecyclerView.Adapter<ApplicationListAdapter.ViewHolder>() {
+
+    val diskManager = DiskManager(context)
+
+    // TODO: make temp vars not properties
+    // TODO: lazy? (especially icons)
+    // TODO: filter on system pkgs? http://stackoverflow.com/a/8483920? enabled apps?
+    private val _installedApps = context.packageManager.getInstalledApplications(0)
+    private val _appsAndStateOnDisk = diskManager.readApplicationsFromDisk()
+    val installedAppInfo = _installedApps.map {
+        val pkgManager = context.packageManager
+        AppInfo(pkgName = it.packageName,
+                label = it.loadLabel(pkgManager).toString(),
+                icon = it.loadIcon(pkgManager),
+                isChecked = _appsAndStateOnDisk.getOrElse(it.packageName, { true }))
+    }.sortedBy { it.label }
+
+    override fun getItemCount(): Int {
+        return installedAppInfo.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val view = LayoutInflater.
+                from(parent!!.context).
+                inflate(R.layout.application_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val app = installedAppInfo[position]
+        holder.title.text = app.label
+        holder.icon.setImageDrawable(app.icon)
+
+        holder.toggle.isChecked = app.isChecked
+        holder.toggle.setOnCheckedChangeListener { buttonView, isChecked ->
+            // Thread-safe: only updated from UI thread.
+            app.isChecked = isChecked
+            diskManager.saveApplicationsToDisk(installedAppInfo.associateBy({it.pkgName}, {it.isChecked})) // TODO: converting each time is slow.
+        }
+    }
+
+    // TODO: This name collides with ApplicationInfo.
+    private data class AppInfo(val pkgName: String, val label: String, val icon: Drawable, var isChecked: Boolean)
+
+    private class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+        val title = itemView!!.findViewById(R.id.title) as TextView
+        val icon = itemView!!.findViewById(R.id.icon) as ImageView
+        val toggle = itemView!!.findViewById(R.id.toggle) as Switch
+    }
+}
